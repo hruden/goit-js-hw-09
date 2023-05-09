8a1b47384f05b54dcbc2acd50e1bf03a98b579b8
hruden <119438895+hruden@users.noreply.github.com>
2023-05-09 17:34:34 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    bodyEl : document.querySelector('body'),
+    btnStart : document.querySelector('button[data-start]'),
+    btnStop : document.querySelector('button[data-stop]'),
+}
+let changeColor = null;
+onStop()
 
+function changeBodyColor (){
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+refs.btnStart.addEventListener('click', onStart)
+
+function onStart(){
+    changeColor = setInterval(changeBodyColor, 1000)
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+}
+refs.btnStop.addEventListener('click', onStop)
+
+function onStop(){
+    clearInterval(changeColor)
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+}
